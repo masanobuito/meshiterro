16941e104d0656d8e4b81e8acf5c820645209f21
Apple <apple@ApplenoMacBook-Pro.local>
2019-07-15 16:47:05 +0900

    [Finish] 5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '953240299a25ef62b48d6d565eb0430f3a423c6d07584a62abf7613aaa6ab50e63da67ca4a5efa985f9888b2ac8a538b827e75f69304370dc732b6b9f7bf9a75'
